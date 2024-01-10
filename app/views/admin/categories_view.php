4c7ab689ac3b9319389cfec2203f546b4e3a74ea
Abderrahman Badi <144786380+Yorften@users.noreply.github.com>
2024-01-10 11:24:14 +0100

    added data tables

--- app/views/admin/categories_view.php
+++ app/views/admin/categories_view.php
@@ -6,7 +6,54 @@
 </head>
 
 <body>
-    categories
+    <div class="flex flex-col justify-end items-start h-[100vh]">
+        <div class="flex justify-between items-center w-full md:px-8">
+            <p class="border-gray-300 rounded-t-lg p-2 pb-1 text-xl">Categories</p>
+            <?php
+            if (isset($msg)) {
+                if ($msg == 1) {
+                    echo '<div class="bg-green-500 mb-3 px-2 rounded-lg">';
+                    echo '<p class="text-white text-lg text-center">Category added successfully!</p>';
+                    echo '</div>';
+                } elseif ($msg == 2) {
+                    echo '<div class="bg-green-500 mb-3 px-2 rounded-lg">';
+                    echo '<p class="text-white text-lg text-center">Category modified successfully!</p>';
+                    echo '</div>';
+                } else {
+                    foreach ($msg as $error) {
+                        echo '<div class="bg-red-500 mb-3 px-2 rounded-lg">';
+                        echo '<p class="text-white text-lg text-center">' . $error . '</p>';
+                        echo '</div>';
+                    }
+                }
+            }
+            ?>
+        </div>
+        <div class="border-2 border-gray-300 rounded-xl w-full h-[90vh] flex">
+            <div id="categories" class="flex flex-col justify-center items-center w-full h-full p-1 md:p-4">
+                <!-- <p class="w-full h-full flex items-center justify-center">No client accounts in database</p> -->
+                <div class="container w-full md:w-11/12 lg:w-[95%] xl:w-3/5 mx-auto px-2">
+                    <div id='recipients' class="py-8 mt-6 lg:mt-0 rounded shadow-xl bg-white">
+                        <table id="table" class="stripe hover" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
+                            <thead>
+                                <tr>
+                                    <th data-priority="1">Id</th>
+                                    <th data-priority="2">Name</th>
+                                    <th data-priority="3">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <!-- loop -->
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php require_once(__DIR__ . '/../components/datatables.php') ?>
 </body>
 
 </html>
--- app/views/components/head.php
+++ app/views/components/head.php
@@ -1,17 +1,72 @@
 <meta charset="UTF-8">
-<meta name="viewport"
-    content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link rel="stylesheet" href="<?php echo ROOT ?>assets/style/output.css">
 <link rel="icon" type="image/ico" href="<?php echo ROOT ?>assets/images/wiki_logo.png">
 <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
 
-<link
-    href="https://fonts.googleapis.com/css2?family=Irish+Grover&family=Lexend&family=Podkova&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-    rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Irish+Grover&family=Lexend&family=Podkova&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+<link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet">
+
+<link href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css" rel="stylesheet">
 <style>
     html {
         font-family: poppins;
+    }
+
+    .dataTables_wrapper select,
+    .dataTables_wrapper .dataTables_filter input {
+        color: #4a5568;
+        padding-left: 1rem;
+        padding-right: 1rem;
+        padding-top: .5rem;
+        padding-bottom: .5rem;
+        line-height: 1.25;
+        border-width: 2px;
+        border-radius: .25rem;
+        border-color: #edf2f7;
+        background-color: #edf2f7;
+    }
+
+    table.dataTable.hover tbody tr:hover,
+    table.dataTable.display tbody tr:hover {
+        background-color: #ebf4ff;
+    }
+
+    .dataTables_wrapper .dataTables_paginate .paginate_button {
+        font-weight: 700;
+        border-radius: .25rem;
+        border: 1px solid transparent;
+    }
+
+    .dataTables_wrapper .dataTables_paginate .paginate_button.current {
+        color: #fff !important;
+        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06);
+        font-weight: 700;
+        border-radius: .25rem;
+        background: #667eea !important;
+        border: 1px solid transparent;
+    }
+
+    .dataTables_wrapper .dataTables_paginate .paginate_button:hover {
+        color: #fff !important;
+        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06);
+        font-weight: 700;
+        border-radius: .25rem;
+        background: #667eea !important;
+        border: 1px solid transparent;
+    }
+
+    table.dataTable.no-footer {
+        border-bottom: 1px solid #e2e8f0;
+        margin-top: 0.75em;
+        margin-bottom: 0.75em;
+    }
+
+    table.dataTable.dtr-inline.collapsed>tbody>tr>td:first-child:before,
+    table.dataTable.dtr-inline.collapsed>tbody>tr>th:first-child:before {
+        background-color: #667eea !important;
     }
 </style>
